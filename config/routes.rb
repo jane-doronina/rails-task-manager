b8fdd546955dfa4dd731a8c4f7391e2ad93bb5f7
Zhanna Doronina <doronina.jane@mail.ru>
2022-08-23 15:57:54 +0100

    added the CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id', to: 'tasks#show', as: :show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 end
